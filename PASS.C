f9bf35fb021a24dd71264f7eeb207c6c16ed5587
sri1729mani <96563843+sri1729mani@users.noreply.github.com>
2021-12-30 11:57:12 +0530

    Add files via upload

---
+++ PASS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("enter the marks");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	if (a>=35 && b>=35 && c>=35 && d>=35)
+     {
+     	printf("PASS");
+	 }
+	 else 
+	 {
+	 	printf("FAIL");
+	 }
+	
+}
