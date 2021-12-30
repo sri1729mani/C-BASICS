6f278fd296f042733bc9d5e154b13fb69d75825f
sri1729mani <96563843+sri1729mani@users.noreply.github.com>
2021-12-30 10:54:21 +0530

    Add files via upload

---
+++ S.c
@@ -0,0 +1,56 @@
+ #include<stdio.h>
+ main()
+ {
+ 	int a,b,c,d,e;
+ 	printf("enter the a");
+ 	scanf("%d",&a);
+ 	b=a%1;
+ 	if (b==0)
+ 	{
+ 		printf("it is divible by 1");
+	}
+	else 
+	{
+		printf("it is divisible by 1");
+	}
+	c=a%2;
+	if (c==0)
+	{
+		printf("it is not divisible by 2");
+	}
+	else
+	{
+		printf("it is not divisible by 2");
+	}
+	d=a%3;
+	if (d==0)
+	{ 
+	printf("it is divisible by 3");
+   }	
+	else 
+	{ 
+	   printf("it is not divisible by 3");
+	}
+	e=a%9;
+	if (e==0)
+	{
+		printf("it isdivisible by 9");
+    }	
+	else 
+	{ 
+	  printf("it is not divisible by 9");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 
+ }
+ 
