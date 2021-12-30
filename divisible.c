996467ca6ebb7dde294efa6759a36bc9562c36b3
sri1729mani <96563843+sri1729mani@users.noreply.github.com>
2021-12-30 10:09:57 +0530

    Add files via upload

---
+++ divisible.c
@@ -0,0 +1,56 @@
+ #include<stdio.h>
+ main()
+ {
+ 	int a,b,c,d,e;
+ 	printf("enter the a");
+ 	scanf("%d",&a);
+ 	b=a%1;
+ 	if (b=a)
+ 	{
+ 		printf("\nit is divible by 1");
+	}
+	else 
+	{
+		printf("\nit is not divisible by 1");
+	}
+	c=a%2;
+	if (c==0)
+	{
+		printf("\nit is divisible by 2");
+	}
+	else
+	{
+		printf("\nit is not divisible by 2");
+	}
+	d=a%3;
+	if (d==0)
+	{ 
+	printf("\nit is divisible by 3");
+   }	
+	else 
+	{ 
+	   printf("\nit is not divisible by 3");
+	}
+	e=a%9;
+	if (e==0)
+	{
+		printf("\nit is divisible by 9");
+    }	
+	else 
+	{ 
+	  printf("\nit is not divisible by 9");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 
+ }
+ 
